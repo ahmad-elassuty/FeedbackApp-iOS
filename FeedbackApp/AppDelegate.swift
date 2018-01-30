68f5236c31c392c51e05b77ccd893d24d4ddcc90
Ahmed Elassuty <ahmad.elassuty@gmail.com>
2018-01-30 15:04:44 +0200

    Setup project
    - Restructure application folders
    - Add cocoaPods for dependency management

---
+++ FeedbackApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  FeedbackApp
+//
+//  Created by Ahmed Elassuty on 1/30/18.
+//  Copyright © 2018 Challenges. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Podfile
+platform :ios, '9.0'
+
+def include_testing_pods
+    pod 'Quick'
+    pod 'Nimble'
+end
+
+target 'FeedbackApp' do
+  use_frameworks!
+end
